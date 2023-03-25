19aa23be440f701f2b3e34f75ce0d12cbff764a2
chang2eee <kb_6537@naver.com>
2023-03-25 20:31:27 +0900

    modified:   PNUwordCorrection.py

--- PNUwordCorrection.py
+++ PNUwordCorrection.py
@@ -5,11 +5,10 @@ import json
 CHECKER_URL = 'http://164.125.7.61/speller/results'
 
 # 입력 파일과 출력 파일의 이름
-INPUT_FILE = 'input.txt'
-OUTPUT_FILE = 'output.txt'
+text_file = 'myInterview.txt'
 
 # 입력 파일에서 텍스트 읽기
-with open(INPUT_FILE, 'r', encoding='utf-8') as f:
+with open(text_file, 'r', encoding='UTF-8') as f:
     text = f.read()
 
 # 개행 문자를 \r\n으로 변환
@@ -28,7 +27,5 @@ for err in data['errInfo']:
     corrected_text = corrected_text.replace(err['orgStr'], err['candWord'])
 
 # 교정된 텍스트를 출력 파일에 쓰기 (원본 파일 덮어쓰기)
-with open(INPUT_FILE, 'w', encoding='utf-8') as f:
+with open(text_file, 'w', encoding='utf-8') as f:
     f.write(corrected_text)
-
-print(f'"{INPUT_FILE}" 파일이 교정되어 "{OUTPUT_FILE}" 파일로 저장되었습니다.')
