fdf4bb908cc35ac69791614583500ee2c2544057
chang2eee <kb_6537@naver.com>
2023-03-25 21:50:59 +0900

    new file:   Face Emotion/readExpression.py 	modified:   Voice And Text/findingKeyword.py

--- Voice And Text/findingKeyword.py
+++ Voice And Text/findingKeyword.py
@@ -16,7 +16,8 @@ def find_most_frequent_keyword(text_file, keyword_list):
     return most_frequent_keyword
 
 # keyword_list 안에 우리가 DB 테이블 명을 넣으면 된다.
-keyword_list = ['자료구조', 'DB']   
+keyword_list = ['네트워크', '데이터베이스', '딥러닝', '머신러닝', '소프트웨어공학',
+                '알고리즘', '운영체제', '지료구조', '정보보안', '코딩', '통계수학']   
 
 result = find_most_frequent_keyword(text_file, keyword_list)
 print(result)
