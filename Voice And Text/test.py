2d19141c318b0a0ddcddeeab39282e2a488a3cbb
chang2eee <kb_6537@naver.com>
2023-04-05 21:20:31 +0900

    modified:   Voice And Text/test.py

--- Voice And Text/test.py
+++ Voice And Text/test.py
@@ -225,6 +225,33 @@ with open(text_file, 'w', encoding='UTF-8') as file:
     # 리스트 요소를 문자열로 결합하여 파일에 쓰기
     file.write(' '.join(word_list))
 
+import pandas as pd
+
+excel_file = 'data_dictionary.xlsx'
+
+df = pd.read_excel(excel_file, header=0) # 엑셀 파일 읽어오기
+target_words = df.iloc[:, 0].tolist() # 첫 번째 열 읽어오기 : 첫 번째 열에는 고칠 단어들이 들어있다.
+corrected_words = df.iloc[:, 1].tolist() # 두 번째 열 읽어오기 : 첫 번째 열에 있는 단어들들 두 번째 열에 있는 단어들로 고쳐야 한다
+
+# txt 파일 읽기
+with open(text_file, "r", encoding="UTF-8") as file:
+    lines = file.readlines()
+
+# 저장한 새로운 list 만들기
+new_lines = []
+
+# txt 파일 수정
+for line in lines:
+    for i, target in enumerate(target_words):
+        corrected_target = corrected_words[i].strip()
+        line = line.replace(target, corrected_target)
+    new_lines.append(line)
+
+# txt 파일 다시 쓰기
+with open(text_file, 'w', encoding='UTF-8') as file:
+    for line in new_lines:
+        file.write(line)
+
 
 # 가장 많이 언급되는 keyword를 내림차순으로 반환 (Line 232 ~ Line 256)
 
